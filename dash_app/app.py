c3777447f88746a75778d239268456a2c992c128
bellissima0419 <myrealgeorgia@gmail.com>
2019-06-27 20:14:08 -0400

    added transitions and leyends

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,38 +1,16 @@
 # FLASK_APP=dash_app/app.py flask run
-import os
 
 import pandas as pd
 import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-
 import sqlite3
 import re
 
 app = Flask(__name__)
 
-#################################################
-# Database Setup SQLAlchemy
-#################################################
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/js_overload.sqlite"
-# db = SQLAlchemy(app)
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(db.engine, reflect=True)
-# Survey = Base.classes.jso11k
-#################################################
 
-############# sqlite3 ##########################
 @app.route("/")
 def index():
-
     """Return the homepage."""
     return render_template("index.html")
 
@@ -56,25 +34,9 @@ def names():
 
     return jsonify(column_names)
 
-    # column names using sqlalchemy:
-
-    # Use Pandas to perform the sql query
-    # stmt = db.session.query(Survey).statement
-    # df = pd.read_sql_query(stmt, db.session.bind)
-
-    # # Return a list of the column names 
-    # return jsonify(list(df.columns))
-
 ####################################################
-    # MAP CHART ROUTE
-    # @TODO route for the map chart   
-# @app.route("/map")
-####################################################
-
-####################################################
-    # DONUT CHART ROUTE
-    # @TODO route for the DONUT chart   
-@app.route("/donut")
+    # COUNTRIES API
+@app.route("/countries")
 def countries():
     """
      Return a list of number of survey respondents by country
@@ -95,63 +57,60 @@ def countries():
 
     for row in rows:
         tempDict = {}
-        tempDict[row[0]] = int(row[1])
+        tempDict["country"] = row[0]
+        tempDict["respondentCount"] = int(row[1])
+        # tempDict[row[0]] = int(row[1])
         country_data.append(tempDict)
 
-
     return jsonify(country_data)
+####################################################
+####################################################
+
+#  SEPARETE ROUTES FOR EACH CHART WHILE IN DEVELOPMENT
+
+####################################################
+    # MAP CHART ROUTE
+    # @TODO route for the map chart
+# THE CSS STYLE IS JUST A DEMO
+@app.route("/mapChart")
+def mapChart():
+    """Return the mapChart page."""
+    return render_template("mapChart.html")
+###################################################
+
+####################################################
+    # LINE CHART ROUTE
+    # @TODO route for the LINE chart
+# THE CSS STYLE IS JUST A DEMO
+@app.route("/lineChart")
+def lineChart():
+    """Return the lineChart page."""
+    return render_template("lineChart.html")
+
 ####################################################
 
 ####################################################
     # LINE CHART ROUTE
-    # @TODO route for the LINE chart   
-# @app.route("/line")
+    # @TODO route for the donutChart chart
+# THE CSS STYLE IS JUST A DEMO
+@app.route("/donutChart")
+def donutChart():
+    """Return the lineChart page."""
+    return render_template("donutChart.html")
+    
+####################################################
 ####################################################
 
-####################################################
-    # DONUT CHART ROUTE
-    # @TODO route for the DONUT chart   
-# @app.route("/donut")
-####################################################
-
-# @app.route("/countries")
-# def countries():
-#     """
-#      Return a list of number of survey respondents by country
-#     """
-#     conn = sqlite3.connect("dash_app/db/js_overload.sqlite")
-#     cur = conn.cursor()
-
-#     query_string = '''
-#         SELECT Country, count(country) FROM jso11k
-#         GROUP BY Country
-#         ORDER BY COUNT(Country) DESC
-#     '''
-
-#     cur.execute(query_string)
-#     rows = cur.fetchall()
-
-#     country_data = []
-
-#     for row in rows:
-#         tempDict = {}
-#         tempDict[row[0]] = int(row[1])
-#         country_data.append(tempDict)
-
-
-#     return jsonify(country_data)
+    # BAR CHART ROUTE
+    # @TODO route for the  BAR chart
+# THE CSS STYLE IS JUST A DEMO
+@app.route("/barChart")
+def barChart():
+    """Return the barChart page."""
+    return render_template("barChart.html")####################################################
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
-
 
 ####################################################
 ####################################################
---
+++ dash_app/static/js/donutChart.js
@@ -0,0 +1,137 @@
+var dimensions = { height: 500, width: 500, radius: 200 };
+var center = { x: (dimensions.width / 2 + 5), y: (dimensions.height / 2 + 5)};
+
+// create svg container
+var svg = d3.select('.donut')
+  .append('svg')
+  .attr('width', dimensions.width + 150)
+  .attr('height', dimensions.height + 150);
+
+var graph = svg.append('g')
+  .attr("transform", `translate(${center.x}, ${center.y})`);
+  // translates the graph group to the middle of the svg container
+
+var pie = d3.pie()
+  // .sort(null)
+  .value(d => d.respondentCount);
+  // the value we are evaluating to create the pie angles
+
+var arcPath = d3.arc()
+  .outerRadius(dimensions.radius)
+  .innerRadius(dimensions.radius / 2.3);
+
+var  color = d3.scaleOrdinal(d3["schemeCategory10"]);
+
+// legend setup
+var legendGroup = svg.append('g')
+  .attr('transform', `translate(${dimensions.width + 40}, 10)`)
+
+var legend = d3.legendColor()
+  .shape('circle')
+  .shapePadding(10)
+  .scale(color)
+
+// update function
+var update = (data) => {
+
+  // update color scale domain
+  color.domain(data.map(d => d.country))
+
+    // update legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text').attr('fill', 'black');
+  
+  // join enhanced (pie) data to path elements
+  var paths = graph.selectAll('path')
+    .data(pie(data));
+      // console.log('pie(data): ',pie(data));
+     // console.log('paths: ', paths);
+  
+  // handle the exit selection
+  paths.exit().remove()
+    .transition().duration(750)
+    .attrTween("d", arcTweenExit)
+    .remove();
+
+  // handle the current DOM path updates
+  paths.attr('d', arcPath)
+    .transition().duration(750)
+    .attrTween('d', arcTweenUpdate)
+
+  paths.enter()
+    .append('path')
+      .attr('class', 'arc')
+      // .attr('d', d => arcPath(d)) //  .attr('d', arcPath) //
+      .attr('stroke', 'grey')
+      .attr('stroke-width', 2)
+      .attr('fill', d => color(d.data.country))
+      .each(function(d){ this._current = d })
+      .transition().duration(750).attrTween("d", arcTweenEnter);
+      
+      // .transition().duration(750).attrTween("d", arcTweenEnter)
+
+};
+
+// data array to hold the api data
+var top10Countries = [];
+
+d3.json('/countries').then( (data) =>  {
+  // console.log('data: ', data);
+  top10 = data.slice(0, 10)
+  // console.log('top10 : ', top10);
+  top10.forEach(element => {
+    top10Countries.push(element)
+  });
+
+  // console.log('top10Countries : ', top10Countries);
+  // console.log('top10[0]: ', top10[0]);
+  // console.log('top10[0].country: ', top10[0].country);
+  
+  update(top10Countries)
+  console.log('top10Countries1: ', top10Countries);
+
+  setTimeout(function(){ 
+    alert("Hello")
+    let first = top10Countries[0].respondentCount
+    let second = top10Countries[1].respondentCount
+    top10Countries[0].respondentCount = second
+    top10Countries[1].respondentCount = first;
+
+    update(top10Countries)
+    console.log('top10Countries2: ', top10Countries);
+
+  }, 750);
+
+});
+
+const arcTweenEnter = (d) => {
+  var i = d3.interpolate(d.endAngle-0.1, d.startAngle);
+
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+}
+
+var arcTweenExit = (d) => {
+  var i = d3.interpolate(d.startAngle, d.endAngle);
+
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+}
+
+function arcTweenUpdate(d) {
+  // console.log('this._current');
+  // console.log('d: ', d)
+  // interpolate between the two objects
+  var i = d3.interpolate(this._current, d);
+  // update the current prop with new updated data
+  this._current = i(1);
+
+  return function(t) {
+    // i(t) returns a value of d (data object) which we pass to arcPath
+    return arcPath(i(t));
+  };
+};
