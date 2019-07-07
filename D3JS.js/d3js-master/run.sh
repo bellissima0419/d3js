65397166fbd64d1e58f757af62f7ead5de8ae868
bellissima0419 <myrealgeorgia@gmail.com>
2019-07-07 18:39:31 -0400

    added navbar & additional html pages

--- D3JS.js/d3js-master/run.sh
+++
@@ -1,1 +0,0 @@
-FLASK_APP=dash_app/app.py flask run
---
+++ dash_app/static/js/app.js
@@ -0,0 +1,14 @@
+// const nav = document.querySelector('#biggernav');
+// let topOfNav = nav.offsetTop;
+
+// function fixNavi() {
+//   if (window.scrollY >= topOfNav) {
+//     document.body.style.paddingTop = nav.offsetHeight + 'px';
+//     document.body.classList.add('fixed-navi');
+//   } else {
+//     document.body.classList.remove('fixed-navi');
+//     document.body.style.paddingTop = 0;
+//   }
+// }
+
+// window.addEventListener('scroll', fixNavi);
